c2b6d213092d4809aa5e08e52e8177ad0f84a407
Philipp Katzenberger <katzenberger.ph+github@gmail.com>
2023-11-07 17:02:10 +0100

    feat(main): Copy code from lesson script

--- main.c
+++ main.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
+#include <windows.h>
+
+DWORD WINAPI ThreadFunc(void* data){
+    // Hier kommt Code für den Thread
+    return 0;
+}
 
 int main()
 {
-    printf("Hello World!\n");
-    return 0;
+    // Thread erzeugen mit CreateThread
+
+    HANDLE threadhandle = CreateThread(NULL, 0, ThreadFunc, NULL, 0, NULL);
+    if(threadhandle){
+        // Wenn die Threaderzeugung erfolgreich war, warte auf Beendigung des Threads
+        WaitForSingleObject(threadhandle, INFINITE);
+    }
 }
